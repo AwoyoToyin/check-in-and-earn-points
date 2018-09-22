ff96ce131b521e323d248042f710fe401128d27a
Awoyo Oluwatoyin <awoyotoyin@gmail.com>
2018-09-22 15:39:23 +0100

    chore: export earning model

--- src/api/resources/core/index.ts
+++ src/api/resources/core/index.ts
@@ -1,7 +1,9 @@
 import AbstractConnector from './connector/abstract';
 import INode from './interface/node.interface';
+import EarningModel from './model/earning';
 
 export {
     AbstractConnector,
+    EarningModel,
     INode,
 };
