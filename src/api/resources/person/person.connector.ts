5bdcf29d8e9ea3be824336936a68a95cd9d7767c
Awoyo Oluwatoyin <awoyotoyin@gmail.com>
2018-09-22 15:40:11 +0100

    feat: check-in method to handle check-in requests

--- src/api/resources/person/person.connector.ts
+++ src/api/resources/person/person.connector.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from 'express';
 
-import { BadRequest, Created, Negotiate, Ok } from '../../responses';
+import { BadRequest, Created, Forbidden, Negotiate, Ok } from '../../responses';
 import { AddressConnector, IAddress } from '../address';
-import { AbstractConnector } from '../core';
+import { CheckInConnector, ICheckIn } from '../checkins';
+import { AbstractConnector, EarningModel } from '../core';
 import { IWallet, WalletConnector } from '../wallet';
 import IPerson from './person.interface';
 import PersonModel from './person.model';
 
 // import { logger } from '../../../config';
-
 class PersonConnector extends AbstractConnector {
 
     constructor() {
@@ -51,6 +51,45 @@ class PersonConnector extends AbstractConnector {
         return this.model.findById(body.owner)
             .then((person: IPerson) => AddressConnector.create(body))
             .then((address: IAddress) => this.model.findOneAndUpdate({ _id: address.owner }, { address: address._id }, { new: true }))
+            .then((person: IPerson) => Ok(res, person))
+            .catch((error: any) => Negotiate(res, error));
+    }
+
+    /**
+     * Person Check-in
+     * @param req Request
+     * @param res Response
+     * @returns Response
+     */
+    public check_in = (req: Request, res: Response): Response => {
+        const body: ICheckIn = req.body || {};
+
+        if (!body.owner || !body.formatted || !body.longitude || !body.latitude) {
+            return BadRequest(res);
+        }
+
+        return this.model.findById(body.owner).populate('address').populate('wallet', 'balance')
+            .then((person: IPerson) => {
+
+                if (!person.address.latitude || !person.address.longitude) {
+                    return Forbidden(res, {}, 'You must set your address first before you can check-in locations');
+                }
+
+                const earning: number[] = new EarningModel()
+                    .getDistance(person.address, body)
+                    .calculate(person.wallet.balance);
+
+                body.distance = earning[1];
+                body.earning = earning[0];
+
+                const balance = person.wallet.balance + body.earning;
+
+                return Promise.all([
+                    WalletConnector.updateBalance(person.wallet._id, balance),
+                    CheckInConnector.create<ICheckIn>(body),
+                ]);
+            })
+            .then((results: [IWallet, ICheckIn]) => this.model.findById(results[0].owner).populate('wallet', 'balance'))
             .then((person: IPerson) => Ok(res, person))
             .catch((error: any) => Negotiate(res, error));
     }
