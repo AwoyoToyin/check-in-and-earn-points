f91093ddd9e508deb3342a5057498660019ef82d
Awoyo Oluwatoyin <awoyotoyin@gmail.com>
2018-09-22 15:34:49 +0100

    refactor: removed conditional statement for attributes

--- src/api/resources/address/address.interface.ts
+++ src/api/resources/address/address.interface.ts
@@ -2,8 +2,8 @@ import { INode } from '../core';
 import { IPerson } from '../person';
 
 export default interface IAddress extends INode {
-    formatted?: string;
-    latitude?: string;
-    longitude?: string;
-    owner?: IPerson;
+    formatted: string;
+    latitude: string;
+    longitude: string;
+    owner: IPerson;
 }
--- src/api/resources/wallet/wallet.interface.ts
+++ src/api/resources/wallet/wallet.interface.ts
@@ -2,6 +2,6 @@ import { INode } from '../core';
 import { IPerson } from '../person';
 
 export default interface IWallet extends INode {
-    balance?: number;
-    owner?: IPerson | string;
+    balance: number;
+    owner: IPerson | string;
 }
