5b3f0722f4cf391e8b297632fa1a82e99cbc8741
Awoyo Oluwatoyin <awoyotoyin@gmail.com>
2018-09-22 12:57:19 +0100

    feat: registered person routes

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import * as bodyParser from 'body-parser';
+import express from 'express';
+
+import { PersonRouter } from './api/resources';
+import { DB } from './config';
+
+class App {
+
+    public app: express.Application;
+
+    constructor() {
+        this.app = express();
+
+        this.openDBConnection();
+        this.config();
+        this.registerRouters();
+    }
+
+    private config(): void {
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    private registerRouters(): void {
+        this.app.use('/person', PersonRouter);
+    }
+
+    private openDBConnection(): void {
+        DB.connect();
+    }
+
+}
+
+export default new App().app;
