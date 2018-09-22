978a39effa5ce730f252ff4daf39d1fb2f6632e8
Awoyo Oluwatoyin <awoyotoyin@gmail.com>
2018-09-22 15:35:57 +0100

    feat: method to update wallet balance

--- src/api/resources/wallet/wallet.connector.ts
+++ src/api/resources/wallet/wallet.connector.ts
@@ -1,10 +1,20 @@
 import { AbstractConnector } from '../core';
+import IWallet from './wallet.interface';
 import WalletModel from './wallet.model';
 
 class WalletConnector extends AbstractConnector {
 
     constructor() {
         super(WalletModel);
+    }
+
+    public updateBalance = async (_id: string, balance: number): Promise<IWallet> => {
+        try {
+            const wallet: IWallet = await this.model.findOneAndUpdate({ _id }, { balance }, { new: true });
+            return wallet;
+        } catch (error) {
+            throw error;
+        }
     }
 
 }
